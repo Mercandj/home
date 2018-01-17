13ada65b6a08c2e29ef3768d30f676c2ffb6981b
Jonathan <modjow.jm9@gmail.com>
2018-01-17 23:51:32 +0100

    [Things][MainActivity] :art: Display pin 7 status. Display wifi ip

--- things/src/main/java/com/mercandalli/android/home/MainActivity.kt
+++ things/src/main/java/com/mercandalli/android/home/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.mercandalli.android.home
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.ContentValues.TAG
+import android.content.Context
+import android.net.wifi.WifiManager
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.widget.TextView
 import com.google.android.things.pio.Gpio
 import com.google.android.things.pio.PeripheralManagerService
 import java.io.IOException
-
+import java.math.BigInteger
+import java.net.InetAddress
+import java.net.UnknownHostException
+import java.nio.ByteOrder
 
 class MainActivity : Activity() {
 
@@ -16,13 +23,19 @@ class MainActivity : Activity() {
     private var value: Boolean = false
     private val handler = Handler()
     private var runnable: Runnable? = null
+    private var gpio7TextView: TextView? = null
 
+    @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         title = "Home Mercandalli"
 
         Log.d("jm/debug", "Java to jni on Android things: " + stringFromJNI())
+
+        findViewById<TextView>(R.id.activity_main_ip)!!.text = "Ip: " + wifiIpAddress(this)
+        gpio7TextView = findViewById(R.id.activity_main_gpio7)
 
         // Attempt to access the GPIO
         try {
@@ -32,14 +45,16 @@ class MainActivity : Activity() {
             Log.w(TAG, "Unable to access GPIO", e)
         }
 
-        runnable = Runnable { doJob() }
+        runnable = Runnable { runnableJob() }
         handler.post(runnable)
     }
 
-    fun doJob() {
+    private fun runnableJob() {
         write(gpio!!, value)
         value = !value
-        Log.d("jm/debug", "Read: " + read(gpio!!))
+        val read = read(gpio!!)
+        gpio7TextView!!.text = if (read) "Gpio7: rp3 output -> ON" else "Gpio7: rp3 output -> OFF"
+        Log.d("jm/debug", "Read: " + read)
         handler.postDelayed(runnable, 1_000)
     }
 
@@ -52,7 +67,6 @@ class MainActivity : Activity() {
                 Log.w(TAG, "Unable to close GPIO", e)
             }
         }
-
         super.onDestroy()
     }
 
@@ -89,6 +103,29 @@ class MainActivity : Activity() {
 
             // Read the active high pin state
             return gpio.value
+        }
+
+        @JvmStatic
+        private fun wifiIpAddress(context: Context): String? {
+            val wifiManager = context.getSystemService(Context.WIFI_SERVICE) as WifiManager
+            var ipAddress = wifiManager.connectionInfo.ipAddress
+
+            // Convert little-endian to big-endianif needed
+            if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) {
+                ipAddress = Integer.reverseBytes(ipAddress)
+            }
+
+            val ipByteArray = BigInteger.valueOf(ipAddress.toLong()).toByteArray()
+
+            val ipAddressString: String?
+            ipAddressString = try {
+                InetAddress.getByAddress(ipByteArray).hostAddress
+            } catch (ex: UnknownHostException) {
+                Log.e("WIFIIP", "Unable to get host address.")
+                null
+            }
+
+            return ipAddressString
         }
     }
 
